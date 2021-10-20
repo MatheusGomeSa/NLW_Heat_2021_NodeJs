9052e5369fc855d04418e27464cf5b4d63084d84
blackmatman <mth.ggss@gmail.com>
2021-10-19 22:16:27 -0300

    Back-end NLW Heat

---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,30 @@
+import prismaClient from "../prisma";
+import { io } from '../app'
+
+class CreateMessageService {
+    async execute(text: string, user_id:string){
+        const message = await prismaClient.message.create({
+            data:{
+                text : text,
+                user_id:user_id,
+            },
+            include:{
+                user:true,
+            }
+        })
+        const infosWS ={
+            text: message.text,
+            user_id:message.user_id,
+            created_at:message.created_at,
+            user:{
+                name:message.user.name,
+                avatar_url:message.user.avatar_url,
+            }
+        }
+
+        io.emit("New_message", infosWS)
+
+        return message;
+    } 
+}
+export { CreateMessageService }
